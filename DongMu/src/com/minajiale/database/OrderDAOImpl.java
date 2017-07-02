f2328289a2894068acb4173d94d6922d96437330
minajiale <599085363@qq.com>
2017-07-02 20:44:03 +0800

    test

---
+++ DongMu/src/com/minajiale/database/OrderDAOImpl.java
@@ -0,0 +1,34 @@
+package com.minajiale.database;
+
+import java.util.List;
+
+import object.order;
+
+public class OrderDAOImpl implements OrderDAO {
+
+	public void addOrder(order oneorder) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void updateOrder(order oneorder) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deleteOrder(int orderId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<order> findAllOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public order findOrderById(int orderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
