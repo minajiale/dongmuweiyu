e21c4c883711f8f4e6b91f96b21306b7e0ded441
minajile <599085363@qq.com>
2017-07-14 10:31:00 +0800

    me

--- DOngMuFront/DongMuFrontend/src/components/view/content/Alarm.vue
+++ DOngMuFront/DongMuFrontend/src/components/view/content/Alarm.vue
@@ -1,25 +1,45 @@
 <template>
   <div class="">
-  <h1>提醒服务</h1>
-  <template>
-    <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
-    <div style="margin: 15px 0;"></div>
-    <div class="" v-for="city in cities">
-      <el-checkbox v-model="checked">{{city}}</el-checkbox>
+    <h1>提醒服务</h1>
+      <div class="check-box-group" >
+      <div class="check-box-group-one">
+        <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
+        <div style="margin: 15px 0;"></div>
+        <el-checkbox-group    v-model="checkedCities" @change="handleCheckedCitiesChange">
+            <el-checkbox class="check-box" v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
+        </el-checkbox-group>
+      </div>
+
+      <div class="check-box-group-two">
+        <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
+        <div style="margin: 15px 0;"></div>
+        <el-checkbox-group    v-model="checkedCities" @change="handleCheckedCitiesChange">
+            <el-checkbox class="check-box" v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
+        </el-checkbox-group>
+      </div>
     </div>
-    <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange">
-      <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
-    </el-checkbox-group>
-  </template>
   </div>
 </template>
+<style>
+  .check-box{
+    display: block;
+  }
+  .check-box-group{
+    display: flex;
+    top: 1000px;
+  }
+</style>
 <script>
-  const cityOptions = ['上海nhbgvfcdxsjyhtgrfdeskjuhygtfcrdxszjuhygtfrds', '北京', '广州', '深圳'];
+  const cityOptions = [
+    '上海nhbgvfcdxsjyhtgrfdeskjuhygtfcrdxszjuhygtfrds',
+    '北京jhgfcdsjhgfcdxsjhgfds',
+    '广州',
+     '深圳'];
   export default {
     data() {
       return {
         checkAll: true,
-        checkedCities: ['上海', '北京'],
+        checkedCities: [],
         cities: cityOptions,
         isIndeterminate: true
       };
