c90e2303816fbec354c24d03d2f278a8e6d72bd7
minajile <599085363@qq.com>
2017-07-14 16:10:22 +0800

    me

--- DOngMuFront/DongMuFrontend/src/components/view/content/classification.vue
+++ DOngMuFront/DongMuFrontend/src/components/view/content/classification.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="">
-    <h1>产品分类管理　&nbsp <a href="#">+</a> </h1>
+    <h1>产品分类管理　
+      &nbsp <el-button type="text" @click="dialogFormVisible = true"
+      style="font-size:40px;margin-left:-30px;">+</el-button></h1>
     <el-input
     class="classificationIn"
     placeholder="输入关键字进行过滤"
@@ -15,6 +17,21 @@
       :filter-node-method="filterNode"
       ref="tree2">
     </el-tree>
+
+    <el-dialog title="新增分类" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="一级分类" :label-width="formLabelWidth">
+          <el-input v-model="form.name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="二级分类" :label-width="formLabelWidth">
+          <el-input v-model="form.name" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -73,7 +90,19 @@
         defaultProps: {
           children: 'children',
           label: 'label'
-        }
+        },
+      dialogFormVisible: false,
+      form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '120px'
       };
     }
   };
