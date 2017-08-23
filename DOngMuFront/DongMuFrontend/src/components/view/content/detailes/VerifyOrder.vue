8cf5031e3c97f81736f7f7eee3b12aa5b2bc4699
minajile <599085363@qq.com>
2017-08-23 17:06:16 +0800

    me

--- DOngMuFront/DongMuFrontend/src/components/view/content/detailes/VerifyOrder.vue
+++ DOngMuFront/DongMuFrontend/src/components/view/content/detailes/VerifyOrder.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="">
+    <div class="personal-info">
+      <h2>   刘联友 </h2><span>赵湾镇 &nbsp &nbsp</span><span>15929159416</span>
+    </div>
+    <div class="">
+      <span>2017-08-23</span>
+    </div>
     <Verify-general></Verify-general>
     <Verify-door></Verify-door>
     <div class="">
@@ -18,10 +24,12 @@
 
 <script>
 import VerifyDoor from './VerifyDoor.vue'
+import VerifyGeneral from './VerifyGeneral.vue'
 
   export default {
     components:{
-      VerifyDoor
+      VerifyDoor,
+      VerifyGeneral
     },
     data() {
       return {
