3155a65678efd0ecd0e42ca80c9b4cd08e3503f9
minajile <599085363@qq.com>
2017-08-29 18:25:14 +0800

    me

--- MEVN/fontend/src/components/view/content/Products.vue
+++ MEVN/fontend/src/components/view/content/Products.vue
@@ -19,23 +19,14 @@
                 <img v-bind:src="props.row.imgSrc" class="image">
               </div></el-col>
                <el-col :span="12"><el-form label-position="left" inline class="demo-table-expand">
-                <el-form-item label="商品名称">
-                  <span>{{ props.row.name }}</span>
-                </el-form-item>
-                <el-form-item label="所属店铺">
-                  <span>{{ props.row.shop }}</span>
-                </el-form-item>
-                <el-form-item label="商品 ID">
+                <el-form-item label="商品ID">
                   <span>{{ props.row.id }}</span>
                 </el-form-item>
-                <el-form-item label="店铺 ID">
-                  <span>{{ props.row.shopId }}</span>
+                <el-form-item label="剩余库存">
+                  <span>{{ props.row.shop }}</span>
                 </el-form-item>
-                <el-form-item label="商品分类">
-                  <span>{{ props.row.category }}</span>
-                </el-form-item>
-                <el-form-item label="店铺地址">
-                  <span>{{ props.row.address }}</span>
+                <el-form-item label="卖价">
+                  <span>{{ props.row.id }}</span>
                 </el-form-item>
                 <el-form-item label="商品描述">
                   <span>{{ props.row.desc }}</span>
@@ -45,15 +36,15 @@
             </template>
           </el-table-column>
           <el-table-column
-            label="商品 ID"
-            prop="id">
-          </el-table-column>
-          <el-table-column
             label="商品名称"
             prop="name">
           </el-table-column>
           <el-table-column
-            label="描述"
+            label="商品货号"
+            prop="name">
+          </el-table-column>
+          <el-table-column
+            label="规格"
             prop="desc">
           </el-table-column>
           <el-table-column label="操作">
