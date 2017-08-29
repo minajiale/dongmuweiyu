3b3cd87ae91e14113544bb75950f1a6d986845ed
minajile <599085363@qq.com>
2017-08-29 18:08:33 +0800

    me

--- MEVN/fontend/src/components/view/content/detailes/ChangeOrder.vue
+++ MEVN/fontend/src/components/view/content/detailes/ChangeOrder.vue
@@ -1,12 +1,25 @@
 <template>
   <el-form :model="form">
-    <el-form-item label="活动名称" :label-width="formLabelWidth">
+    <el-form-item label="日期" :label-width="formLabelWidth">
       <el-input v-model="form.name" auto-complete="off"></el-input>
     </el-form-item>
-    <el-form-item label="活动区域" :label-width="formLabelWidth">
-      <el-select v-model="form.region" placeholder="请选择活动区域">
-        <el-option label="区域一" value="shanghai"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
+    <el-form-item label="顾客姓名" :label-width="formLabelWidth">
+      <el-input v-model="form.name" auto-complete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="顾客联系方式" :label-width="formLabelWidth">
+      <el-input v-model="form.name" auto-complete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="订单总金额" :label-width="formLabelWidth">
+      <el-input v-model="form.name" auto-complete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="欠款金额" :label-width="formLabelWidth">
+      <el-input v-model="form.name" auto-complete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="订单状态" :label-width="formLabelWidth">
+      <el-select v-model="form.region" placeholder="请选择订单状态">
+        <el-option label="完成" value="shanghai"></el-option>
+        <el-option label="预付定金" value="beijing"></el-option>
+        <el-option label="欠款" value="beijing"></el-option>
       </el-select>
     </el-form-item>
   </el-form>
