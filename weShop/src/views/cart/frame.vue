a8c1234461bac8049cafadb760616e973cf1b12b
minajile <599085363@qq.com>
2017-11-04 19:08:34 +0800

    me

---
+++ weShop/src/views/cart/frame.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <address></address>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {},
+  ready () {},
+  attached () {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="css">
+</style>
