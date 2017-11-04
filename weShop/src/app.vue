645895351f61a1e7c378c78d6cc163dd159157d8
minajile <599085363@qq.com>
2017-11-04 17:54:03 +0800

    me

---
+++ weShop/src/app.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  
+  <div class="weui-flex">
+    <div class="weui-flex__item"><div class="placeholder">weui</div></div>
+    <div class="weui-flex__item"><div class="placeholder">weui</div></div>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {},
+  ready () {},
+  attached () {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ weShop/src/views/homepage/sidebar.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <ul>
+    <li v-for="item in classifications">{{item}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      classifications:[
+        {
+          name:"电热水器"
+        },
+        {
+          name:"烟机"
+        }
+      ]
+    }
+  },
+  computed: {},
+  ready () {},
+  attached () {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="css">
+li{
+  list-style-type: none;
+  border-bottom: 1px solid gray
+}
+</style>
