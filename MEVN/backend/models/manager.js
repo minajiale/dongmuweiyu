bc16da227db129f20e364143091b4206be7739ec
minajile <599085363@qq.com>
2018-01-17 21:10:15 +0800

    me

--- MEVN/backend/models/manager.js
+++ MEVN/backend/models/manager.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 var managerSchema = new Schema({
   "username":String,
   "password":String,
+  "role":Boolean,//0表示店主,1表示店员
 });
 //将该Schema发布为Model
 module.exports = mongoose.model("manager",managerSchema)
