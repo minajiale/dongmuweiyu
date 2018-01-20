6b39673b91b71a214f2f2a2753a995269b619483
minajile <599085363@qq.com>
2018-01-20 08:20:29 +0800

    me

--- MEVN/fontend/src/App.vue
+++ MEVN/fontend/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <login></login>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import login from './components/Login.vue'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    login,
+  }
 }
 </script>
 
